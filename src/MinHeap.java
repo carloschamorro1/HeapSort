676a0f59391718398f5480eb1f754867bc80bd40
Carlos <cmchamorro99@hotmail.com>
2019-07-06 13:58:52 -0600

    Creando metodo heapify

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -1,12 +1,30 @@
 public class MinHeap {
     DynArray dynArray = new DynArray();
 
+    public void swap(int [] arr, int i, int j){
+        int temp  = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public void heapify(int[] arr, int n, int i){
+        int mayor = i;
+        int izquierda = (2 * i) + 1;
+        int derecha = (2*i) + 2;
 
-    private int flotar(){
-        return 0;
+        if(izquierda < n && arr[izquierda] > arr[mayor]){
+            mayor = izquierda;
+        }
+
+        if(derecha< n && arr[derecha] > arr[mayor]){
+            mayor = derecha;
+        }
+
+        if(mayor != i){
+            swap(arr,n,mayor);
+            heapify(arr,mayor,i);
+        }
+
+
     }
 
-    private void hundir(){
-
-    }
 }
