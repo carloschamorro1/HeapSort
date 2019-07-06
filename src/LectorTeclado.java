376f8c1abc5ccb5679f0766bec96ce276d244f60
Carlos <cmchamorro99@hotmail.com>
2019-07-06 14:13:59 -0600

    Agregando clase de lector  de enteros

---
+++ src/LectorTeclado.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LectorTeclado {
+    private Scanner mSc;
+
+    public LectorTeclado() {
+        Scanner sc = new Scanner(System.in);
+        init(sc);
+    }
+
+    private void init(Scanner sc) {
+        mSc = sc;
+    }
+    
+    public int leerEntero(String mensaje, String mensajeError) {
+        int retval = 0;
+        System.out.println(mensaje);
+        while (!mSc.hasNextInt()) {
+            mSc.nextLine();
+            System.out.println(mensajeError);
+        }
+        retval = mSc.nextInt();
+        mSc.nextLine();
+        return retval;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        //Creamos una referencia a nuestro arreglo dinamico
         DynArray dynArray = new DynArray();
+        Random random = new Random();
 
+        System.out.println("Bienvenido");
+        System.out.println("Ingrese con cuantos elementos desea que se llene el arreglo de manera aleatoria");
+
+
+        for (int i = 0; i <dynArray.arr.length; i++) {
+            System.out.println(dynArray.arr[i]);
+        }
+//        MinHeap minHeap = new MinHeap();
+//        minHeap.sort();
     }
 }
